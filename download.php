3a00140dcf19c96f267aa6237c63e6ca1822e6f3
fame <projects.mvut@gmail.com>
2017-08-23 09:59:20 +0530

    new

---
+++ download.php
@@ -0,0 +1,36 @@
+<?php
+require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'info@mindvis.in';                 // SMTP username
+$mail->Password = 'cbieu1234';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('info@mindvis.in', 'Skool Diaries');
+$mail->addAddress('bharatbgarg4@gmail.com');     // Add a recipient
+$mail->addAddress('himanshu.vasistha@gmail.com');               // Name is optional
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Skool Diaries';
+
+$body='';
+foreach( $_POST as $a => $v ) {
+	$body=$body.'<p>'.$a.': <b>'.$v.'</b></p>';
+}
+$mail->Body    = $body;
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+    header('Location: /Brochure.pdf');
+}
